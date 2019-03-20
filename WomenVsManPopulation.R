283754f8c90691d76ff95b5d4f0a8d0a8427001e
Michael Stevan <miestgo@gmail.com>
2019-03-20 20:19:34 +0100

    4 continents done

--- WomenVsManPopulation.R
+++ WomenVsManPopulation.R
@@ -10,7 +10,6 @@ library(data.table)
 library(ggplot2)
 library(gridExtra)
 
-
 dt = fread("WPP2017_TotalPopulationBySex.csv")
 
 # Casting
@@ -23,60 +22,63 @@ summary(dt)
 # Use meaningful columns
 dt[,`:=`(LocID=NULL,VarID=NULL)]
 
-# TODO: Use medium variant value
-# Average across location and time, to get rid of variants
-dt_avg  = copy(dt[,.(avgPopMale=mean(PopMale),
-                avgPopFemale = mean(PopFemale),
-                avgPopTotal = mean(PopTotal)),
-                .(Location,Time)])
+# Use medium variant
+dt  = dt[Variant=="Medium"]
 
 # Get unique rows
-dt_avg = unique(dt_avg)
+dt = unique(dt)
 
-# Paint avgPopFemale over the continents
-ggplot(data=dt_avg[Location%in%c("Africa")],aes(x=Time,y=avgPopFemale))+
+# Paint PopFemale over the continents
+ggplot(data=dt[Location%in%c("China")],aes(x=Time,y=PopFemale))+
   geom_line(col="red")+
-  geom_line(aes(y=avgPopMale),col="blue")+
+  geom_line(aes(y=PopMale),col="blue")+
   theme_minimal()+
-  ylab("Population")
-  ggtitle("Africa")
+  ylab("Population")+
+  ggtitle("China")
 
-p1 = ggplot(data=dt_avg[Location=="Europe"],aes(x=Time,y=avgPopFemale))+
+p1 = ggplot(data=dt[Location=="Europe"],aes(x=Time,y=PopFemale))+
   geom_line(col="red")+
-  geom_line(aes(y=avgPopMale),col="blue")+
+  geom_line(aes(y=PopMale),col="blue")+
   theme_minimal()+
   ylab("Population")+
   ggtitle("Europe")
 
-p2 = ggplot(data=dt_avg[Location=="Asia"],aes(x=Time,y=avgPopFemale))+
+p2 = ggplot(data=dt[Location=="Asia"],aes(x=Time,y=PopFemale))+
   geom_line(col="red")+
-  geom_line(aes(y=avgPopMale),col="blue")+
+  geom_line(aes(y=PopMale),col="blue")+
   theme_minimal()+
   ylab("Population")+
   ggtitle("Asia")
 
-p3 = ggplot(data=dt_avg[Location=="Africa"],aes(x=Time,y=avgPopFemale))+
+p3 = ggplot(data=dt[Location=="Africa"],aes(x=Time,y=PopFemale))+
   geom_line(col="red")+
-  geom_line(aes(y=avgPopMale),col="blue")+
+  geom_line(aes(y=PopMale),col="blue")+
   theme_minimal()+
   ylab("Population")+
   ggtitle("Africa")
 
-p4 = ggplot(data=dt_avg[Location=="America"],aes(x=Time,y=avgPopFemale))+
+
+dt_america = dt[Location%in%c("Central America","South America","Northern America")]
+dt_america = dt_america[,.(PopMale = mean(PopMale),PopFemale=mean(PopFemale)),.(Time)]
+
+p4 = ggplot(data=dt_america,aes(x=Time,y=PopFemale))+
   geom_line(col="red")+
-  geom_line(aes(y=avgPopMale),col="blue")+
+  geom_line(aes(y=PopMale),col="blue")+
   theme_minimal()+
   ylab("Population")+
   ggtitle("America")
 
 
 
+print(p1)
+print(p2)
+print(p3)
+print(p4)
 
 gA <- ggplotGrob(p1)
 gB <- ggplotGrob(p2)
 gC <- ggplotGrob(p3)
 gD <- ggplotGrob(p4)
-
 
 maxWidth = grid::unit.pmax(gA$widths[2:5], gB$widths[2:5],gC$widths[2:5],gD$widths[2:5])
 gA$widths[2:5] <- as.list(maxWidth)
@@ -107,12 +109,12 @@ plot(p5)
 # Add new interesting columns:
 
 # Female to Male ratio, How many women per men eper year?
-dt_avg[,avgFemToMaleRatio:=avgPopFemale/avgPopMale,.(Location,Time)]
+dt[,avgFemToMaleRatio:=PopFemale/PopMale,.(Location,Time)]
 
 # How have the ratio of women per men varied in several countries?
-dt_avg_sel_countries = copy(dt_avg[Location%in%c("Spain","France","Potugal","Italy","Germany","United Kingdom","United States of America","Qatar")])
+dt_sel_countries = copy(dt[Location%in%c("Spain","France","Potugal","Italy","Germany","United Kingdom","United States of America","Qatar")])
 
-ggplot(data=dt_avg_sel_countries,aes(x=Time,y=avgFemToMaleRatio,col=Location))+
+ggplot(data=dt_sel_countries,aes(x=Time,y=avgFemToMaleRatio,col=Location))+
   geom_line(size=3,alpha=0.65)+
   theme_minimal()+
   ggtitle("Female to male ratio over the years")
@@ -125,22 +127,22 @@ continents = c("Africa",
             "Asia",
             "Australia")
 
-dt_avg_continent = copy(dt_avg[Location%in%continents])
+dt_continent = copy(dt[Location%in%continents])
 
-ggplot(data=dt_avg_continent,aes(x=Time,y=avgFemToMaleRatio,col=Location))+
+ggplot(data=dt_continent,aes(x=Time,y=avgFemToMaleRatio,col=Location))+
   geom_line(size=3,alpha=0.65)+
   theme_minimal()+
   ggtitle("Female to male ratio over the years per continent")
 
 
 # Which countries have the most women per men?
-dt_avg_ftm_location= copy(dt_avg)
-dt_avg_ftm_location = dt_avg_ftm[,FemToMaleRatio:=mean(avgFemToMaleRatio),.(Location)][order(-FemToMaleRatio)]
+dt_ftm_location= copy(dt)
+dt_ftm_location = dt_ftm[,FemToMaleRatio:=mean(avgFemToMaleRatio),.(Location)][order(-FemToMaleRatio)]
 
-head(dt_avg_ftm_location,20)
+head(dt_ftm_location,20)
 
 # Which countries have the least women per men?
-head(dt_avg_ftm_location[order(FemToMaleRatio)],20)
+head(dt_ftm_location[order(FemToMaleRatio)],20)
 
 # Paint in a world map:
 # Bubble Plot
