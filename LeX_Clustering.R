b56140a8b42dc308cb96b6d2369eb6012dfc3646
manuel <manuelcarranza96@hotmail.com>
2019-03-25 01:30:23 +0100

    Progress clustering

--- LeX_Clustering.R
+++ LeX_Clustering.R
@@ -19,9 +19,23 @@ dt[,Time:=apply(dt[,.(Time)],1,function(x){as.numeric(unlist(strsplit(x,"-"))[2]
 # Remove rows with any null
 dt = dt[complete.cases(dt),]
 
-# Transpose table
-dt_trans = dcast(dt, Location ~ Time  , value.var = "LEx")
+# Remove rows of future prediction
+dt_past = dt[Time<2020]
 
+# Transpose table
+dt_trans = dcast(dt_past, Location ~ Time  , value.var = "LEx")
+
+rows_to_remove = c("World","Africa","Eastern Africa","Middle Africa",
+                   "Northern Africa", "Southern Africa","Western Africa",
+                   "Asia","Eastern Asia","South-Central Asia","Central Asia", "Southern Asia",
+                   "South-Eastern Asia", "Western Asia",
+                   "Europe","Eastern Europe","Northern Europe",
+                   "Southern Europe","Western Europe",
+                   "Caribbean", "Latin America and the Caribbean", "Central America",
+                   "South America","Northern America","Oceania",
+                   "Australia/New Zealand","Melanesia","Micronesia","Polynesia")
+
+dt_trans = dt_trans[!Location %in% rows_to_remove]
 
 ## Get numeric table (Remove location)
 dt_numeric = copy(dt_trans)[,Location:=NULL]
@@ -36,25 +50,48 @@ max_val <- max(dt_trend)
 min_val <- min(dt_trend)
 
 dt_trend_norm <- apply(dt_trend,2,function(x){(x-min_val)/(max_val-min_val)})
+#dt_trend_norm <- dt_trend
 
-## Optimal num of clusters
-fviz_nbclust(dt_trend_norm, kmeans, method = "wss")
-
-
-
-num_clusters <- 8
 # Clustering
+## Optimal num of clusters
+f <- fviz_nbclust(dt_trend_norm, kmeans, method = "wss")
+plot(f)
+
+num_clusters <- 3
+
 clusters <- kmeans(dt_trend_norm, num_clusters)
 clusters$tot.withinss
 
-## 6 clusters Camboya única en cluster 2 fila 33
-dt$cluster <- clusters$cluster
 
-#group<-compareGroups(cluster~.,data=dt_numeric)
+fv <- fviz_cluster(clusters, geom = "point", data = dt_trend) +  ggtitle(paste("2D Cluster solution (k=", num_clusters, ")", sep=""))
+plot(fv)
+
+## 6 clusters Camboya única en cluster 2 fila 33
+#dt$cluster <- clusters$cluster
+dt_trend[,Location:=dt_trans[,Location]]
+dt_trend[,cluster:=clusters$cluster]
+
+
+plot_tendencies_by_cluster<- function(dt_trend,cluster){
+  dt_trend[,cluster:=clusters$cluster]
+  trend_cluster <- dt_trend[,!"Location"]
+  trend_cluster <- dt_trend_mean_cluster[, lapply(.SD, mean), by=cluster]
+  cnames <- colnames(trend_cluster)
+  cnames <- cnames[cnames!="cluster"]
+  melted <- melt(trend_cluster, measure.vars = cnames)
+  melted$variable <- as.numeric(as.character(melted$variable))
+  ggplot(melted, aes(variable,value, col=as.factor(cluster))) + geom_line(size=1.5)
+  
+}
+
+
+plot_tendencies_by_cluster(dt_trend,cluster)
+#group<-compareGroups(cluster~.,data=dt_trend, max.ylev=12, max.xlev = 21)
 #clustab<-createTable(group)
 #print(clustab)
 
 mapDevice('x11')
-spdf <- joinCountryData2Map(dt, joinCode="NAME", nameJoinColumn="Location")
+spdf <- joinCountryData2Map(dt_trend, joinCode="NAME", nameJoinColumn="Location")
 mapCountryData(spdf, nameColumnToPlot="cluster", addLegend=TRUE,
                catMethod="fixedWidth",numCats = num_clusters,colourPalette="diverging")
+
--- LeX_Graphs.R
+++ LeX_Graphs.R
@@ -36,26 +36,59 @@ dt = dt[complete.cases(dt),]
 # Transpose table
 dt_trans = dcast(dt, Location ~ Time  , value.var = "LEx")
 
+rows_to_remove = c("World","Africa","Eastern Africa","Middle Africa",
+                   "Northern Africa", "Southern Africa","Western Africa",
+                   "Asia","Eastern Asia","South-Central Asia","Central Asia", "Southern Asia",
+                   "South-Eastern Asia", "Western Asia",
+                   "Europe","Eastern Europe","Northern Europe",
+                   "Southern Europe","Western Europe",
+                   "Caribbean", "Latin America and the Caribbean", "Central America",
+                   "South America","Northern America","Oceania",
+                   "Australia/New Zealand","Melanesia","Micronesia","Polynesia",
+                   "More developed regions","Less developed regions","Least developed countries",
+                   "Less developed regions, excluding least developed countries",
+                   "Less developed regions, excluding China", "High-income countries",
+                   "Middle-income countries","Upper-middle-income countries",
+                   "Lower-middle-income countries", "Low-income countries",
+                   "Sub-Saharan Africa"
+)
+
+dt_trans = dt_trans[!Location %in% rows_to_remove]
 
 ## Get numeric table (Remove location)
 dt_numeric = copy(dt_trans)[,Location:=NULL]
 
 ## Transform table, get diff between consecutive periods
-f <- function (x){diff(x)}
+
 cols <- tail(colnames(dt_numeric),-1)
-dt_trend <- apply(dt_numeric,1, f)
+dt_trend <- apply(dt_numeric,1, function (x){diff(x)})
 dt_trend <- data.table(t(dt_trend))
 
 ## Get countries with very negative tendencies
-indices <- data.frame(which(dt_trend <= -5, arr.ind=T))$row
+indices_min <- unique(data.frame(which(dt_trend <= -5, arr.ind=T))$row)
 # rows_with_neg <- apply(dt_trend, 1, function(r) any(r<0))
 
 ## Countries with highest negative trends
-countries_neg_trends <- dt_trend[indices,]
+countries_neg_trends <- dt_trend[indices_min,]
 countries_neg_trends$min_trend <- apply(countries_neg_trends,1,min)
-countries <- dt_trans[indices, ]
-countries$min_trend <- countries_neg_trends$min_trend
-top_neg_countries <- countries[order(min_trend)]
+countries_neg <- dt_trans[indices_min, ]
+countries_neg$min_trend <- countries_neg_trends$min_trend
+top_neg_countries <- countries_neg[order(min_trend)]
+
+
+## Get countries with very positive tendencies
+indices_max <- unique(data.frame(which(dt_trend >= 7, arr.ind=T))$row)
+# rows_with_neg <- apply(dt_trend, 1, function(r) any(r<0))
+
+## Countries with highest positive trends
+countries_pos_trends <- dt_trend[indices_max,]
+countries_pos_trends$max_trend <- apply(countries_pos_trends,1,max)
+countries_pos <- dt_trans[indices_max, ]
+countries_pos$max_trend <- countries_pos_trends$max_trend
+top_pos_countries <- countries_pos[order(max_trend,decreasing = TRUE)]
+top_pos_countries <- top_pos_countries[!Location %in% c("Rwanda","Cambodia","Zimbabwe","Timor-Leste","Botswana")]
+
+
 
 
 plot_country_lex <- function (dt, location){
@@ -105,14 +138,14 @@ plot_country_gender_lex <- function (dt, location){
   
 }
 
-plot_country_lex(dt,"Rwanda")
-plot_country_gender_lex(dt,"Rwanda")
+#plot_country_lex(dt,"Germany")
+#plot_country_gender_lex(dt,"Rwanda")
 #ggsave(paste0("Rwanda","_gender.png"),width=1192,height=649,limitsize = FALSE)
 
 
 
 
-plot_countries_lex <- function (dt, locations){
+plot_countries_min_lex <- function (dt, locations){
   plt <- ggplot()
   for (loc in locations){
     country <- dt[Location==loc & Time<2020,.(Location,Time,LEx)]
@@ -138,7 +171,41 @@ plot_countries_lex <- function (dt, locations){
   plt
 }
 
-plot_countries_lex(dt,head(top_neg_countries[,Location],5))
+
+plot_countries_max_lex <- function (dt, locations){
+  plt <- ggplot()
+  for (i in seq(length(locations))){
+    loc <- locations[i]
+    country <- dt[Location==loc & Time<2020,.(Location,Time,LEx)]
+    country_head <- country[1:dim(country)[1]-1]
+    
+    country_trend <- dcast(country, Location ~ Time  , value.var = "LEx")[,!"Location"]
+    cols <- tail(colnames(country_trend),-1)
+    country_trend <- apply(country_trend,1, function (x){diff(x)})
+    country_trend  <- data.table(t( country_trend))
+    index_max_increase <- which(country_trend == max(country_trend), arr.ind=T)[1,2] + 1
+
+    plt <- plt + geom_line(data=country, aes(x=Time, y=LEx,col=Location),size=1.5) +
+      geom_point(data=country[index_max_increase,], aes(x=Time, y=LEx), colour="black", size=5)
+  }
+  plt <- plt + geom_line(size=1.5)+
+    xlab("Year") + ylab("Life Expectancy")+
+    theme(plot.title = element_text(face="bold", size=20))+
+    theme(axis.title.x = element_text(size=18)) +
+    theme(axis.title.y = element_text(size=18)) +
+    theme(axis.text.x = element_text(size=18))+
+    theme(axis.text.y = element_text(size=18)) +
+    theme(legend.text=element_text(size=16)) +
+    theme(legend.title=element_text(size=16)) +
+    theme(legend.position="bottom", legend.box = "horizontal") +
+    scale_x_continuous(breaks=seq(1950, 2020, by = 5))+
+    scale_y_continuous(breaks=seq(0,100, by = 10)) +
+    guides(linetype=F)+
+    ggtitle("Life Expectancy")
+  plt
+}
+
+plot_countries_max_lex(dt,head(top_pos_countries[,Location],5))
 
 
 
