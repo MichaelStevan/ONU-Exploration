9b7686b29daaf0eb8871a20fbcbf8a10fa03a012
manuel <manuelcarranza96@hotmail.com>
2019-03-25 17:18:24 +0100

    Clustering casi terminado

--- LeX_Clustering_Trend.R
+++ LeX_Clustering_Trend.R
@@ -51,16 +51,26 @@ dt_clustering <- apply(dt_numeric,2,function(x){(x-min_val)/(max_val-min_val)})
 ## Optimal number of clusters
 # fviz_nbclust(dt_clustering, kmeans, method = "wss")
 
+wss <- 0
+for (i in 1:10) {
+  km.out <- kmeans(dt_clustering, i, nstar=5)
+  wss[i] <- km.out$tot.withinss
+}
 
-# WSS
+plot(1:10, wss, type = "b", xlab = "Number of Clusters",
+     ylab = "Within groups sum of squares")
+
+
+
 index <- 0
 for (i in 2:10) {
   km.out <- kmeans(dt_clustering, i, nstar=5)
-  #index[i] <- dunn(clusters=km.out$cluster, Data = dt_clustering, method = "euclidean")
-  index[i-1] <- km.out$tot.withinss
+  index[i] <- dunn(clusters=km.out$cluster, Data = dt_clustering, method = "euclidean")
+  #index[i-1] <- km.out$tot.withinss
+  #dis <- dist(dt_clustering)^2
+  #index[i] <- silhouette(km.out$cluster,dis)
 }
 
-## Silhouette, Dunn, DB
 nb <- NbClust(dt_clustering, diss = NULL, distance = "euclidean", min.nc = 2, max.nc = 10,method="kmeans")
 index <- data.frame(nb$All.index[,"Silhouette"])
 colnames(index) <- "index"
@@ -78,32 +88,74 @@ ggplot(data=index,aes(x=seq(2,10),y=index)) + geom_line(color="cadetblue4",size=
   #scale_y_continuous(breaks=seq(0,100, by = 10)) +
   guides(linetype=F)+
   ggtitle("Silhouette")
-  
-num_clusters <- 5
+
+num_clusters <- 4
 clusters <- kmeans(dt_numeric, num_clusters,nstar=10)
-dt_trans$cluster <- clusters$cluster
 
 
 fv <- fviz_cluster(clusters, geom = "point", data = dt_numeric) +  ggtitle(paste("2D Cluster solution (k=", num_clusters, ")", sep=""))
 plot(fv)
 
-
+dt_trans$cluster <- clusters$cluster
 mapDevice('x11')
 spdf <- joinCountryData2Map(dt_trans, joinCode="NAME", nameJoinColumn="Location")
 mapCountryData(spdf, nameColumnToPlot="cluster", addLegend=TRUE,
                catMethod="fixedWidth",numCats = num_clusters,colourPalette="diverging")
 
+## Optimal number of clusters
+# fviz_nbclust(dt_clustering, kmeans, method = "wss")
+
+# Compute clValid
+# clmethods <- c("hierarchical","kmeans","pam")
+# intern <- clValid(dt_clustering, nClust = 2:6,
+#clMethods = clmethods, validation = "internal")
+# Summary
+#summary(intern)
 
 
+## Transform table, get diff between consecutive periods
+f <- function (x){diff(x)}
+cols <- tail(colnames(dt_numeric),-1)
+dt_trend <- apply(dt_numeric,1, f)
+dt_trend <- data.table(t(dt_trend))
 
-plot_tendencies_by_cluster<- function(dt_trans){
-  trend_cluster <- dt_trans[,!"Location"]
+max_val <- max(dt_trend)
+min_val <- min(dt_trend)
+
+dt_trend_norm <- apply(dt_trend,2,function(x){(x-min_val)/(max_val-min_val)})
+#dt_trend_norm <- dt_trend
+
+# Clustering
+## Optimal num of clusters
+f <- fviz_nbclust(dt_numeric, kmeans, method = "silhouette")
+plot(f)
+
+num_clusters <- 5
+
+clusters <- kmeans(dt_numeric, num_clusters)
+clusters$tot.withinss
+
+
+fv <- fviz_cluster(clusters, geom = "point", data = dt_numeric) +  ggtitle(paste("2D Cluster solution (k=", num_clusters, ")", sep=""))
+plot(fv)
+
+## 6 clusters Camboya única en cluster 2 fila 33
+#dt$cluster <- clusters$cluster
+dt_trend[,Location:=dt_trans[,Location]]
+dt_trend[,cluster:=clusters$cluster]
+
+
+plot_tendencies_by_cluster<- function(dt_trend,cluster){
+  dt_trend[,cluster:=clusters$cluster]
+  dt_trend <- dt_trend[Location!="Cambodia"]
+  trend_cluster <- dt_trend[,!"Location"]
+  
   trend_cluster <- trend_cluster[, lapply(.SD, mean), by=cluster]
   cnames <- colnames(trend_cluster)
   cnames <- cnames[cnames!="cluster"]
   melted <- melt(trend_cluster, measure.vars = cnames)
   melted$variable <- as.numeric(as.character(melted$variable))
-  ggplot(melted, aes(variable,value, col=as.factor(cluster))) + geom_line(size=1.5) +
+  ggplot(melted, aes(variable,value, col=as.factor(cluster))) + geom_line(size=1.5)+
     xlab("Year") + ylab("Life Expectancy")+
     theme(plot.title = element_text(face="bold", size=20))+
     theme(axis.title.x = element_text(size=18)) +
@@ -117,12 +169,18 @@ plot_tendencies_by_cluster<- function(dt_trans){
     scale_y_continuous(breaks=seq(30,100, by = 10)) +
     guides(linetype=F)+
     ggtitle("Life Expectancy by cluster")
+  
 }
 
 
-plot_tendencies_by_cluster(dt_trans)
+plot_tendencies_by_cluster(dt_trend,cluster)
 #group<-compareGroups(cluster~.,data=dt_trend, max.ylev=12, max.xlev = 21)
 #clustab<-createTable(group)
 #print(clustab)
+
+mapDevice('x11')
+spdf <- joinCountryData2Map(dt_trend, joinCode="NAME", nameJoinColumn="Location")
+mapCountryData(spdf, nameColumnToPlot="cluster", addLegend=TRUE,
+               catMethod="fixedWidth",numCats = num_clusters,colourPalette="diverging")
 
 
