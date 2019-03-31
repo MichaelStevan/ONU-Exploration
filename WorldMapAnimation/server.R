c7a64b098fe26a341b48503a6e5c69cdf29f97d7
Michael Stevan <miestgo@gmail.com>
2019-03-31 13:23:48 +0200

    Changed Population map for Life expectancy map

--- WorldMapAnimation/server.R
+++ WorldMapAnimation/server.R
@@ -1,6 +1,5 @@
 library(data.table)
 library(ggplot2)
-library(rgdal)
 library(shiny)
 
 shinyServer(function(input, output) {
@@ -8,14 +7,14 @@ shinyServer(function(input, output) {
   # Map precomputations
   dt = fread('../data/poblacion_tratada_mapa.csv')
   dt[,Location:=factor(Location)]
-  min_pop = min(dt$PopTotal)
-  max_pop = max(dt$PopTotal)
+  min_Lex = min(dt$LEx,na.rm = T)
+  max_Lex = max(dt$LEx,na.rm = T)
   
   output$world_map <- renderPlot({
     
     mapYear = input$year
     
-    dtYear = dt[Time==mapYear, .(Location,PopTotal)]
+    dtYear = dt[Time==mapYear, .(Location,LEx)]
 
     map.world = fread('../data/map_world.csv')
     setkey(map.world,Location)
@@ -23,9 +22,10 @@ shinyServer(function(input, output) {
     map.world_joined = dtYear[map.world,allow.cartesian=T]
     
     ggplot() +
-      geom_polygon(data = map.world_joined, aes(x = long, y = lat, group = group, fill = PopTotal)) +
-      scale_fill_distiller(palette = "Spectral", na.value = "transparent")+
-      labs(title = 'Population by Countries',subtitle = mapYear, fill="") +
+      geom_polygon(data = map.world_joined, aes(x = long, y = lat, group = group, fill = LEx)) +
+      scale_fill_distiller(palette = "Spectral", na.value = "transparent",limits=c(min_Lex,max_Lex))+
+      labs(title = 'Life Expectancy by Countries'
+           ,subtitle = mapYear, fill="") +
       theme(text = element_text(family = "Gill Sans", color="#444444" )
             ,panel.grid = element_blank()
             ,plot.title = element_text(size = 20)
@@ -35,6 +35,7 @@ shinyServer(function(input, output) {
             ,axis.ticks = element_blank()
             ,legend.position = "right"
       )
+    
   })
   
 })
--- WorldMapAnimation/ui.R
+++ WorldMapAnimation/ui.R
@@ -3,15 +3,16 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("World Population"),
+  titlePanel("World's Life Expectancy"),
   
   # Sidebar with a slider input for number of bins 
     
        sliderInput("year",
                    "Year",
-                   min = 1950,
+                   min = 1955,
                    max = 2100,
-                   value = 2017,
+                   value = 2015,
+                   step = 5,
                    animate = T),
     
     # Show a plot of the generated distribution
